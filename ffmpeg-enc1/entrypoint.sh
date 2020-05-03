6beaa22f1d5af4cb0917b10654cf1a65b7ab9862
Jamie Fletcher <jamie@unified-streaming.com>
2020-05-03 18:06:48 +0200

    working dual_ingest setup
    Signed-off-by: Jamie Fletcher <jamie@unified-streaming.com>

--- ffmpeg-enc1/entrypoint.sh
+++ ffmpeg-enc1/entrypoint.sh
@@ -34,9 +34,22 @@ exec ffmpeg -re \
 -i smptehdbars=size=1280x720 \
 -i "https://raw.githubusercontent.com/unifiedstreaming/live-demo/master/ffmpeg/usp_logo_white.png" \
 -filter_complex \
-"[v]drawbox=y=25: x=iw/2-iw/7: c=0x00000000@1: w=iw/3.5: h=36: t=fill, \
+"sine=frequency=1:beep_factor=480:sample_rate=48000, \
+atempo=0.5[a1]; \
+sine=frequency=1:beep_factor=960:sample_rate=48000, \
+atempo=0.5, \
+adelay=1000[a2]; \
+[a1][a2]amix, \
+highpass=40, \
+adelay='$(date +%3N)', \
+asplit=2[a1][a2]; \
+[a1]showwaves=mode=p2p:colors=white:size=1280x100:scale=lin:rate=$(($FRAME_RATE))[waves]; \
+color=size=1280x100:color=black[blackbg]; \
+[blackbg][waves]overlay[waves2]; \
+[0][waves2]overlay=y=620[v]; \
+[v]drawbox=y=25: x=iw/2-iw/7: c=0x00000000@1: w=iw/3.5: h=36: t=fill, \
 drawtext=text='DASH-IF Live Media Ingest Protocol': fontsize=32: x=(w-text_w)/2: y=75: fontsize=32: fontcolor=white,\
-drawtext=text='video-720p25-1000k': fontsize=32: x=(w-text_w)/2: y=125: fontsize=32: fontcolor=white, \
+drawtext=text='Multi Encoder Ingest - Encoder 1 (video-720p25-1000k)': fontsize=32: x=(w-text_w)/2: y=125: fontsize=32: fontcolor=white, \
 drawtext=timecode_rate=${FRAME_RATE}: timecode='$(date -u +%H\\:%M\\:%S)\\${FRAME_SEP}$(($(date +%3N)/$((1000/$FRAME_RATE))))': tc24hmax=1: fontsize=32: x=(w-tw)/2+tw/2: y=30: fontcolor=white, \
 drawtext=text='%{gmtime\:%Y-%m-%d}\ ': fontsize=32: x=(w-tw)/2-tw/2: y=30: fontcolor=white[v+tc]; \
 [v+tc][1]overlay=eval=init:x=W-15-w:y=15[vid]" \
@@ -48,4 +61,12 @@ drawtext=text='%{gmtime\:%Y-%m-%d}\ ': fontsize=32: x=(w-tw)/2-tw/2: y=30: fontc
 -movflags +frag_keyframe+empty_moov+separate_moof+default_base_moof \
 -video_track_timescale 10000000 \
 -ism_offset $(($(date +%s)*10000000)) \
--f mp4 "$PUB_POINT/Streams(video-720p25-1000k.cmfv)" 
+-f mp4 "$PUB_POINT/Streams(video-720p25-1000k.cmfv)" \
+-map "[a2]" -c:a aac -b:a 128k  -metadata:s:a:0 language=dutch \
+-fflags +genpts \
+-frag_duration $AUDIO_FRAG_DUR_MICROS \
+-min_frag_duration $AUDIO_FRAG_DUR_MICROS \
+-movflags +empty_moov+separate_moof+default_base_moof \
+-video_track_timescale 48000 \
+-ism_offset $(($(date +%s)*48000)) \
+-f mp4  "$PUB_POINT/Streams(audio-aac-128k.cmfa)"
